de8ee6995c7c82ff71565e7fb1f7e4beafff3aac
Timo Rantalaiho <timo.rantalaiho@reaktor.com>
2022-06-05 00:23:09 +0300

    format: Run go fmt
    No functional changes.

--- main.go
+++ main.go
@@ -1,50 +1,50 @@
 package main
 
 import (
-  "bufio"
-  "fmt"
-  "os"
-  "strconv"
-  "strings"
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
 )
 
 type Song struct {
-  name            string
-  durationSeconds int
+	name            string
+	durationSeconds int
 }
 
 func main() {
-    fmt.Println("Hello World!")
-    fileContents := readFile()
-    fmt.Println(parseSongs(fileContents))
+	fmt.Println("Hello World!")
+	fileContents := readFile()
+	fmt.Println(parseSongs(fileContents))
 }
 
 func readFile() []string {
-  file, err := os.Open("songs_with_durations.txt")
-  if err != nil {
-    fmt.Println(err)
-    panic(err)
-  }
-  defer file.Close()
+	file, err := os.Open("songs_with_durations.txt")
+	if err != nil {
+		fmt.Println(err)
+		panic(err)
+	}
+	defer file.Close()
 
-  var lines []string
-  scanner := bufio.NewScanner(file)
-  for scanner.Scan() {
-    lines = append(lines, scanner.Text())
-  }
-  return lines
+	var lines []string
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+	return lines
 }
 
 func parseSongs(lines []string) []Song {
-  var songs = make([]Song, len(lines))
-  for i := 0; i < len(lines); i++ {
-    songs[i] = parseSong(lines[i])
-  }
-  return songs
+	var songs = make([]Song, len(lines))
+	for i := 0; i < len(lines); i++ {
+		songs[i] = parseSong(lines[i])
+	}
+	return songs
 }
 
 func parseSong(line string) Song {
-  fields := strings.Split(line, "\t")
-  durationSeconds, _  := strconv.Atoi(fields[1])
-  return Song{fields[0], durationSeconds}
+	fields := strings.Split(line, "\t")
+	durationSeconds, _ := strconv.Atoi(fields[1])
+	return Song{fields[0], durationSeconds}
 }
